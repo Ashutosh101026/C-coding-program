e84309b41055df9abeff3c182f355115a0289e22
Ashutosh <amehta2_be24@thapar.edu>
2024-10-19 22:20:53 +0530

    Create 3_string.c
    use of gets and puts

---
+++ 3_string.c
@@ -0,0 +1,8 @@
+// use of gets and puts
+#include<stdio.h>
+int main(){
+  char st[40];
+  gets(st);// enter string is stored in st....
+  puts(st);// prints the string and shift the cursor to next line..
+  return 0;
+}
